2797ff6dd0a5c500037134a18766c9c7860d1e93
MurodjohnKhayitov <murodillaxayitov@gmail.com>
2023-07-26 14:54:08 +0500

    data
    data

--- src/components/Reviews/ReviewDetails/Carousels/DetailCarousel.js
+++ src/components/Reviews/ReviewDetails/Carousels/DetailCarousel.js
@@ -161,64 +161,66 @@ const DetailCarousel = () => {
   return (
     <div className="w-full h-fit border border-red-500">
       <section className="w-full flex flex-col flex-wrap h-fit gap-x-[10px] ">
-        <article className=" h-[650px] w-full overflow-hidden">
-          <Slider
-            className="w-full h-full rounded-lg"
-            asNavFor={nav2}
-            ref={slider1}
-            {...settings}
-          >
-            {imgGroup?.map((data) => {
-              return (
-                <article key={data?.id}>
-                  {/* <figure className=" w-full h-full overflow-hidden border border-searchBgColor bg-btnBgColor rounded-lg flex items-center justify-center"> */}
-                  <img className="w-full h-fit" src={data?.img} alt="" />
-                  <figcaption className="flex md:hidden w-full absolute items-center justify-between px-4 opacity-80 text-sm font-AeonikProMedium left-0 right-0 bottom-4 ">
-                    <span className="bg-bgCard pt-1 gap-x-[3px] rounded-[40%] px-3 py-1 flex items-center leading-5 tracking-wider  ">
-                      <p> {data.id}</p>/<p>{imgGroup.length}</p>
-                    </span>
-                    <span className="w-fit flex items-center p-2 gap-x-2 rounded-lg bg-bgCard border border-searchBgColor">
-                      <p className="flex items-center ">
-                        <GrFormPrevious />
-                      </p>
-                      <p className="flex items-center not-italic font-AeonikProRegular text-sm leading-4 text-black">
-                        Видео
-                      </p>
-                    </span>
-                  </figcaption>
-                  {/* </figure> */}
-                </article>
-              );
-            })}
-          </Slider>
-        </article>
-        <article className="flex flex-row  w-full mt-4 border border-red-500">
-          <Slider
-            asNavFor={nav1}
-            ref={slider2}
-            // slidesToShow={5}
-            swipeToSlide={true}
-            focusOnSelect={true}
-            vertical={false}
-            {...settings1}
-            className="flex flex-row flex-wrap pt-0 rounded-lg"
-          >
-            {imgGroup?.map((data) => {
-              return (
-                <figure
-                  key={data?.id}
-                  className="!w-[72px] cursor-pointer !h-[96px] border border-searchBgColor bg-btnBgColor rounded-lg flex items-center justify-center"
-                >
-                  <img
-                    className="w-full h-full rounded-lg"
-                    src={data?.img}
-                    alt=""
-                  />
-                </figure>
-              );
-            })}
-          </Slider>
-        </article>
+        <div className="w-full flex flex-col">
+          <div>
+            <Slider
+              className="w-full h-full rounded-lg"
+              asNavFor={nav2}
+              ref={slider1}
+              {...settings}
+            >
+              {imgGroup?.map((data) => {
+                return (
+                  <article key={data?.id}>
+                    <figure className="relative w-full h-full overflow-hidden border border-searchBgColor bg-btnBgColor rounded-lg flex items-center justify-center">
+                      <img className="w-full h-fit" src={data?.img} alt="" />
+                      {/* <figcaption className="flex md:hidden w-full absolute items-center justify-between px-4 opacity-80 text-sm font-AeonikProMedium left-0 right-0 bottom-4 ">
+                        <span className="bg-bgCard pt-1 gap-x-[3px] rounded-[40%] px-3 py-1 flex items-center leading-5 tracking-wider  ">
+                          <p> {data.id}</p>/<p>{imgGroup.length}</p>
+                        </span>
+                        <span className="w-fit flex items-center p-2 gap-x-2 rounded-lg bg-bgCard border border-searchBgColor">
+                          <p className="flex items-center ">
+                            <VideoStoreIcons />
+                          </p>
+                          <p className="flex items-center not-italic font-AeonikProRegular text-sm leading-4 text-black">
+                            Видео
+                          </p>
+                        </span>
+                      </figcaption> */}
+                    </figure>
+                  </article>
+                );
+              })}
+            </Slider>
+          </div>
+          <div>
+            <Slider
+              asNavFor={nav1}
+              ref={slider2}
+              // slidesToShow={5}
+              swipeToSlide={true}
+              focusOnSelect={true}
+              vertical={false}
+              {...settings1}
+              className="flex flex-row flex-wrap pt-0 rounded-lg"
+            >
+              {imgGroup?.map((data) => {
+                return (
+                  <figure
+                    key={data?.id}
+                    className="!w-[72px] cursor-pointer !h-[96px] border border-searchBgColor bg-btnBgColor rounded-lg flex items-center justify-center"
+                  >
+                    <img
+                      className="w-full h-full rounded-lg"
+                      src={data?.img}
+                      alt=""
+                    />
+                  </figure>
+                );
+              })}
+            </Slider>
+          </div>
+        </div>
       </section>
     </div>
   );
