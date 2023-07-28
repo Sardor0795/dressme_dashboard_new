b181821efd6a2fd35b82790b740953552bb1dd19
main <ismoilergashovich@gmail.com>
2023-07-28 09:52:26 +0500

    new changes

--- src/components/Navbar/NavbarForSetting.js
+++ src/components/Navbar/NavbarForSetting.js
@@ -4,7 +4,7 @@ import { NavbarUserIcon } from "../../assets/icons";
 export default function NavbarForSetting() {
   return (
     <div className="w-full h-[80px] border border-lightBorderColor flex items-center justify-end border-l-0 bg-lightBgColor px-[40px]">
-      <button className="w-[56px] h-[56px] rounded-full border border border-lightBorderColor bg-white flex items-center justify-center cursor-pointer">
+      <button className="w-[56px] h-[56px] rounded-full border border-lightBorderColor bg-white flex items-center justify-center cursor-pointer">
         <NavbarUserIcon colors={""} />
       </button>
     </div>
