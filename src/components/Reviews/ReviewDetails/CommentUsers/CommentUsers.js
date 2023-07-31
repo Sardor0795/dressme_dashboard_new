9250ee84d4e9a9b247d2befffad2015056c9db77
main <ismoilergashovich@gmail.com>
2023-07-31 10:34:39 +0500

    new changes

--- src/components/Reviews/ReviewDetails/CommentUsers/CommentUsers.js
+++ src/components/Reviews/ReviewDetails/CommentUsers/CommentUsers.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { CloseAnswer, StarOutlineIcon } from "../../../../assets/icons";
+import { CloseAnswer, MobileStar, SearchIcon, SendIcon, StarOutlineIcon } from "../../../../assets/icons";
 
 export default function CommentUsers() {
 
@@ -62,48 +62,64 @@ export default function CommentUsers() {
     },
   ]);
   return (
-    <div className="w-full h-full  flex flex-col gap-y-[15px]">
-      <div className="pb-1 justify-end text-tableTextTitle2 text-xl not-italic font-AeonikProMedium flex items-center gap-x-4">
-        <p> Отзывы клиентов</p>
+    <div className="w-full h-full  flex flex-col md:gap-y-[15px]">
+      <div className="pb-1 md:justify-end text-tableTextTitle2 text-xl not-italic font-AeonikProMedium flex items-center md:gap-x-4 mt-[37px] mb-[18px] md:mt-0 md:mb-0">
+        <p className="mr-[10px] md:ml-0"> Отзывы клиентов</p>
+        <span className="block md:hidden text-xs text-mobileTextColor">(12 отзывы) </span>
+      </div>
+      <div className="flex md:hidden gap-x-[14px] mb-4">
+        <div className="w-[70%] h-[31px] overflow-hidden border border-lightBorderColor flex items-center px-[10px] rounded">
+          <input
+            type="text"
+            className="w-full h-full text-[13px] outline-0	"
+            placeholder="Поиск"
+          />
+          <span> <SearchIcon /> </span>
+        </div>
+        <button className="w-[30%] h-[31px] bg-textBlueColor flex items-center justify-center text-white rounded px-[8px] ls:px-[10px]">
+          <span className="text-[10px] ls:text-[12px] not-italic font-AeonikProMedium mr-[5px]">
+          Отправить
+          </span>
+          <MobileStar />
+        </button>
       </div>
       {userInfo.map((data) => {
         return (
           <div className="w-full h-fit border border-lightBorderColor rounded-[5px] p-[15px]">
             {/* userImg and Date */}
-            <div className="w-full p-[15px]  h-fit flex justify-between">
+            <div className="w-full md:p-[15px] mb-5 md:mb-0 h-fit flex justify-between">
               <div className="h-10 w-fit flex items-center gap-x-[15px]">
                 <figure className="w-10 h-10  rounded-full overflow-hidden flex items-center justify-center">
                   <img src={data?.userImg} alt="" className="w-full h-full" />
                 </figure>
                 <div className="flex flex-col">
-                  <p className="text-tableTextTitle2 text-xl not-italic font-AeonikProMedium">
+                  <div className="text-tableTextTitle2 text-base md:text-xl font-AeonikProMedium">
                     {data?.userName}
-                  </p>
-                  <p className="flex gap-x-[10px]">
-                    <p className="text-gray-700 text-sm not-italic font-AeonikProRegular leading-normal	">
+                  </div>
+                  <div className="flex md:gap-x-[10px]">
+                    <p className="text-gray-700 text-[13px] md:text-sm font-AeonikProRegular leading-normal">
                       Оценка покупки
                     </p>
-                    <p className="flex items-center gap-x-[2px]">
-                      <span className="text-gray-700 text-sm not-italic font-AeonikProRegular leading-normal	">
-                        {" "}
+                    <p className="flex items-center gap-x-[2px] ml-[5px] md:ml-0">
+                      <span className="text-gray-700 text-[13px] md:text-sm mr-[2px] font-AeonikProRegular leading-normal	">
                         {data?.starCount}.0
-                      </span>{" "}
+                      </span>
                       <span>
                         <StarOutlineIcon />
                       </span>
                     </p>
-                  </p>
+                  </div>
                 </div>
               </div>
-              <div className="h-10 w-fit flex items-center">
-                <span className="text-textLightColor text-base not-italic font-AeonikProRegular leading-normal">
+              <div className="h-10 w-fit flex items-start md:items-center">
+                <span className="text-textLightColor text-xs md:text-base font-AeonikProRegular leading-normal">
                   {data?.date}
                 </span>
               </div>
             </div>
             {/* userText and  */}
-            <div className="p-[15px] w-[95%] h-fit ">
-              <span className="text-gray-700 text-base not-italic font-AeonikProRegular leading-normal">
+            <div className="md:p-[15px] w-full md:w-[95%] h-fit ">
+              <span className="text-mobileTextColor text-[13px] md:text-base not-italic font-AeonikProRegular leading-normal">
                 {data?.userFeedback}
               </span>
             </div>
@@ -111,19 +127,18 @@ export default function CommentUsers() {
               return (
                 <>
                   {item?.subjectReply.length !== 0 ? (
-                    <div className="w-full h-fit  mt-[15px] p-[15px]">
-                      <div className="w-full h-fit flex  justify-between p-[25px] bg-ProductReplyBg rounded-[5px] gap-x-[15px]">
+                    <div className="w-full h-fit mt-[20px] md:mt-[15px] md:p-[15px]">
+                      <div className="w-full h-fit flex justify-between px-[15px] py-3 md:p-[25px] bg-ProductReplyBg rounded-[5px] gap-x-[15px]">
                         <div>
-                          {" "}
-                          <p className="text-tableTextTitle2 text-base not-italic font-AeonikProMedium">
+                          <p className="text-tableTextTitle2 text-[12px] md:text-base font-AeonikProMedium mb-4">
                             {item?.subjectBrand}
                           </p>
-                          <p className="text-gray-700 text-base not-italic font-AeonikProRegular">
+                          <p className="text-gray-700 text-[12px] md:text-base font-AeonikProRegular">
                             {item?.subjectReply}
                           </p>
                         </div>
-                        <div>
-                          <span className="text-textLightColor text-base not-italic font-AeonikProRegular leading-normal">
+                        <div className="flex items-start mt-[2px]">
+                          <span className="text-textLightColor text-[11px] md:text-base font-AeonikProRegular leading-normal">
                             {item?.replyDate}
                           </span>
                         </div>
@@ -131,14 +146,15 @@ export default function CommentUsers() {
                     </div>
                   ) : null}
                   {item?.subjectReply.length !== 0 ? null : (
-                    <div className="w-full h-fit mt-[5px] flex justify-end">
+                    <div className="w-full h-fit mt-[25px] md:mt-[5px] flex justify-end">
                       {sendAnswer ? (
                         <div className="w-full flex items-center justify-between">
-                          <textarea name="answer" id="answer" className="w-4/5 border rounded p-3" placeholder="Add your answer..."></textarea>
-                          <button className="w-[132px] h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg">
-                            <span className="text-sm not-italic font-AeonikProMedium">
+                          <textarea name="answer" id="answer" className="w-4/5 h-12 text-[13px] md:text-base md:h-14 border rounded p-3 md:mr-[20px] xxl:mr-[30px]" placeholder="Add your answer..."></textarea>
+                          <button className="w-[132px] py-2 md:py-0 md:h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg md:mr-[10px]">
+                            <span className="text-[13px] md:text-sm not-italic font-AeonikProMedium">
                             Отправить
                             </span>
+                            {/* <SendIcon /> */}
                           </button>
                           <button
                             onClick={() => setSendAnswer(false)}
@@ -150,8 +166,8 @@ export default function CommentUsers() {
                       : (
                         <button 
                           onClick={() => setSendAnswer(true)}
-                          className="w-[132px] h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg">
-                          <span className="text-sm not-italic font-AeonikProMedium">
+                          className="w-full md:w-[132px] py-2 md:py-0 md:h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg">
+                          <span className="text-[13px] md:text-sm not-italic font-AeonikProMedium">
                           Ответить
                           </span>
                         </button>
