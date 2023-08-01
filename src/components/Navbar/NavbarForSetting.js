24da224f42a2fedf5e9b95bdca4eb456af388e3f
main <ismoilergashovich@gmail.com>
2023-08-01 09:55:31 +0500

    new changes

--- src/components/Navbar/NavbarForSetting.js
+++ src/components/Navbar/NavbarForSetting.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavbarUserIcon, SearchIcon } from "../../assets/icons";
 import MobileHumburgerMenu from "./mobileHamburgerMenu/MobileMenu";
 
 export default function NavbarForSetting() {
+
+  const [searchBtn, setSearchBtn] = useState(false)
+
   return (
-    <div className="w-full pt-6 pb-3 md:py-0 md:h-[80px] border-b md:border border-lightBorderColor flex items-center justify-end border-l-0 md:bg-lightBgColor px-4 md:px-[40px]">
-      
-      {/* Mobile Main Hamburger Menu */}
-      <MobileHumburgerMenu/>
-
+    <>
       {/* Mobile Main Menu Search Section */}
-      <div className="ml-auto block md:hidden mr-4 cursor-pointer">
-        <SearchIcon />
-      </div>
-      
-      {/* Mobile Main Menu User Section */}
-      <button className="md:w-[56px] md:h-[56px] rounded-full md:border border-lightBorderColor bg-white flex items-center justify-center cursor-pointer">
-        <NavbarUserIcon colors={""} />
-      </button>
-
-    </div>
-  );
+        {searchBtn ? (
+          <div className="w-full pt-6 pb-3 md:py-0 md:h-[80px] border-b md:border border-lightBorderColor flex items-center md:justify-end border-l-0 md:bg-lightBgColor px-4 md:px-[40px]">
+            <div className="w-full flex items-center justify-between md:justify-end">  
+              {/* Mobile Main Hamburger Menu */}
+              <MobileHumburgerMenu />
+              <div className="flex items-center">
+                  <div className="block md:hidden mr-4 cursor-pointer">
+                    <SearchIcon />
+                  </div>   
+                  {/* Mobile Main Menu User Section */}
+                  <button className="md:w-[56px] md:h-[56px] rounded-full md:border border-lightBorderColor bg-white flex items-center justify-center cursor-pointer">
+                    <NavbarUserIcon colors={""} />
+                  </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="w-full pt-6 pb-3 md:py-0 md:h-[80px] border-b md:border border-lightBorderColor flex items-center md:justify-end border-l-0 md:bg-lightBgColor px-4 md:px-[40px]">
+            
+            
+            <div className="w-full flex md:hidden items-center justify-between">
+              {/* Mobile Main Hamburger Menu */}
+              <MobileHumburgerMenu />
+              
+              {/* Mobile Main Menu User Section */}
+              <div 
+                onClick={() => setSearchBtn(false)}
+                className="w-full ml-[30px] h-9 overflow-hidden border border-lightBorderColor flex items-center px-[10px] rounded-lg">
+                  <input  
+                    type="text"
+                    className="w-full h-full text-[13px]"
+                    placeholder="Поиск"
+                  />
+                  <span> <SearchIcon /> </span>
+              </div>
+              {/* Desktop Main Header User Section */}
+              <button className="md:w-[56px] md:h-[56px] rounded-full md:border border-lightBorderColor bg-white flex items-center justify-center cursor-pointer">
+                <NavbarUserIcon colors={""} />
+              </button>
+            </div>
+          </div>
+        )}
+    </>
+  )
 }
--- src/components/Navbar/mobileHamburgerMenu/MobileMenu.js
+++ src/components/Navbar/mobileHamburgerMenu/MobileMenu.js
@@ -1,12 +1,87 @@
-import React from 'react'
-import { MobileNavMenu } from '../../../assets/icons'
+import { Button, Modal } from 'antd';
+import { ClothesIcons, MobileNavMenu, NavbarMarketIcon, NavbarReviewIcon } from '../../../assets/icons'
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function MobileHumburgerMenu() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className='block md:hidden'>
-        <div className='cursor-pointer'>
-            <MobileNavMenu />
-        </div>
+    <div className='flex md:hidden items-center'>
+        <button type="primary" onClick={showModal}>
+        <MobileNavMenu />
+        </button>
+        <Modal 
+            open={isModalOpen} 
+            onOk={handleOk}
+            onCancel={handleCancel}
+            // closeIcon={false}
+            footer={null}
+        >
+            <div className="w-full flex flex-wrap gap-y-5">
+                <NavLink
+                    className={
+                    "w-full h-[54px] gap-x-[15px] px-[25px] bg-lightBorderColor rounded-lg flex items-center justify-start"
+                    }
+                    style={({ isActive }) => ({
+                    color: isActive ? "#007DCA" : "#000",
+                    background: isActive ? "#f2f2f2" : "#fcfcfc",
+                    })}
+                    to={"/reviews"}
+                >
+                    <span>
+                    <NavbarReviewIcon colors={""} />
+                    </span>
+                    <span className=" text-lg not-italic font-AeonikProMedium leading-5">
+                    Отзывы
+                    </span>
+                </NavLink>
+                <NavLink
+                    className={
+                    "w-full h-[54px] gap-x-[15px] px-[25px] flex items-center justify-start capitalize bg-lightBorderColor rounded-lg"
+                    }
+                    style={({ isActive }) => ({
+                    color: isActive ? "#007DCA" : "#000",
+                    background: isActive ? "#f2f2f2" : "#fcfcfc",
+                    })}
+                    to={"/shops"}
+                >
+                    <span>
+                    <NavbarMarketIcon colors={""} />
+                    </span>
+                    <span className=" text-lg not-italic font-AeonikProMedium leading-5">
+                    Магазины
+                    </span>
+                </NavLink>
+                <NavLink
+                    className={
+                    "w-full h-[54px] gap-x-[15px] px-[25px] flex items-center justify-start capitalize bg-lightBorderColor rounded-lg"
+                    }
+                    style={({ isActive }) => ({
+                    color: isActive ? "#007DCA" : "#000",
+                    background: isActive ? "#f2f2f2" : "#fcfcfc",
+                    })}
+                    to={"/products"}
+                >
+                    <span>
+                    <ClothesIcons colors={""} />
+                    </span>
+                    <span className=" text-lg not-italic font-AeonikProMedium leading-5">
+                    Одежда
+                    </span>
+                </NavLink>
+            </div>
+        </Modal>
     </div>
   )
 }
