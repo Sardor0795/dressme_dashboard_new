3ae319f079ad74f85abc4a8bb6ed98bff13048cd
main <ismoilergashovich@gmail.com>
2023-08-04 10:40:38 +0500

    new changes in product new page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,7 +3,7 @@ import ProductsPageOne from "./AddingProductPageTwo/ProductsPageOne";
 
 export default function Products() {
   return (
-    <main className="w-full px-4 md:px-10 md:py-[33px]">
+    <main className="products w-full px-4 md:px-10 md:py-5">
       <ProductsPageOne/>
     </main>
     )
