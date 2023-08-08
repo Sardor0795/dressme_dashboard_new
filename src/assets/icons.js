cdfb48c9170dcfcdfdf29478e0f8ce6554ae31f3
main <ismoilergashovich@gmail.com>
2023-08-08 11:51:42 +0500

    added mobile version of products page

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -378,12 +378,19 @@ const StarLabel = () => {
     </svg>
   )
 }
-
 const DownloadIcon = () => {
   return(
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M12 16L7 11L8.4 9.55L11 12.15V4H13V12.15L15.6 9.55L17 11L12 16ZM6 20C5.45 20 4.979 19.804 4.587 19.412C4.195 19.02 3.99934 18.5493 4 18V15H6V18H18V15H20V18C20 18.55 19.804 19.021 19.412 19.413C19.02 19.805 18.5493 20.0007 18 20H6Z" fill="#007DCA"/>
     </svg>
+  )
+}
+const ArrowRightIcon = () => {
+  return(
+    <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 9L5 5L1 1" stroke="#8C8C8C"/>
+    </svg>
+
   )
 }
 
@@ -407,4 +414,5 @@ export {
   InputCheck,
   StarLabel,
   DownloadIcon,
+  ArrowRightIcon,
 };
--- src/components/Products/AddingProductPageOne/ProductsPageTop/ProductsPageTop.js
+++ src/components/Products/AddingProductPageOne/ProductsPageTop/ProductsPageTop.js
@@ -6,23 +6,18 @@ const ProductsPageTop = () => {
     
   return (
     <div className="w-full pt-6 pb-4 md:pt-0 md:pb-5 md:border-b border-lightBorderColor block">
-        <div className="flex justify-end items-center md:justify-between">
-          <section className="hidden md:block">
+        <div className="flex items-center justify-center md:justify-between">
+          <section className="block">
             <div className="text-black text-2xl not-italic font-AeonikProMedium">
             Добавить продукт
             </div>
           </section>
-          <section className="mobileDate flex items-center gap-x-[30px]">
+          <section className="hidden mobileDate md:flex items-center gap-x-[30px]">
             <Space direction="vertical" size={12}>
               <RangePicker />
             </Space>
           </section>
         </div>
-        <section className="flex md:hidden pt-6">
-          <p className="text-black text-2xl not-italic font-AeonikProMedium">
-            С возвращением, Абдулазиз!
-          </p>
-        </section>
       </div>
   )
 }
