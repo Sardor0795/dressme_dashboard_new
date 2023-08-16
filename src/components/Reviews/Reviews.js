50a1aa73983fa836b7002d977b534a454d422d1b
MurodjohnKhayitov <murodillaxayitov@gmail.com>
2023-08-16 16:06:00 +0500

    data
    data

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -153,39 +153,23 @@ export default function Reviews() {
   return (
     <div className="w-full h-full px-4 md:px-10 py-1">
       <div className="w-full pt-6 pb-4 md:py-4 md:border-b border-lightBorderColor block">
-        <div className="flex justify-end items-center md:justify-between">
-          <section className="hidden md:block">
-            <p className="text-black text-2xl not-italic font-AeonikProMedium">
-              С возвращением, Абдулазиз!
-            </p>
-          </section>
-          <section className="mobileDate flex items-center gap-x-[30px]">
-            <Popover
-              open={state?.openwear}
-              onOpenChange={handleOpenChangeWear}
-              className="hidden md:flex items-center justify-between w-[244px] h-10 overflow-hidden cursor-pointer border border-lightBorderColor px-[10px] rounded-lg "
-              trigger="click"
-              options={["Hide"]}
-              placement="bottom"
-              content={contentWear}
-            >
-              <p className="text-textLightColor text-sm not-italic font-AeonikProMedium">
-                Фильтр рейтинг
-              </p>
-              <span>
-                <BiChevronDown
-                  size={20}
-                  style={{ color: "#c2c2c2" }}
-                  className={`${
-                    state?.openwear ? "rotate-[-180deg]" : ""
-                  } duration-200`}
-                />
-              </span>
-            </Popover>
+        <div className="flex  items-center justify-between">
+          <div className="max-w-[400px] w-[100%] h-10 overflow-hidden border border-lightBorderColor flex items-center px-[10px] rounded-lg">
+            <input
+              type="text"
+              className="w-full h-full  outline-0	"
+              placeholder="Поиск"
+            />
+            <button>
+              <SearchIcon />
+            </button>
+          </div>
+          <div>
+            {" "}
             <Space direction="vertical" size={12}>
               <RangePicker />
             </Space>
-          </section>
+          </div>
         </div>
         <section className="flex md:hidden pt-6">
           <p className="text-black text-2xl not-italic font-AeonikProMedium">
