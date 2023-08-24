54c85fca54f266ade9945b526d45b45a412b2d79
main <ismoilergashovich@gmail.com>
2023-08-24 11:50:45 +0500

    new changes

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import { Space, DatePicker } from "antd";
-import { SearchIcon } from "../../assets/icons";
-import { AiOutlineLeft } from "react-icons/ai";
+import { Outlet } from "react-router-dom";
 
-const { RangePicker } = DatePicker;
 
 export default function Products() {
-  const navigate = useNavigate();
 
   return (
     <main className="products w-full px-4 md:px-10 md:py-5">
