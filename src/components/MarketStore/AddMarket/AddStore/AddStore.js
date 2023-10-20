ace6f4164a01666653517d7027d95a6292a62773
Sardor0795 <english.programmer.9595@gmail.com>
2023-10-20 17:26:59 +0500

    sdas

--- src/components/MarketStore/AddMarket/AddStore/AddStore.js
+++ src/components/MarketStore/AddMarket/AddStore/AddStore.js
@@ -359,7 +359,7 @@ function AddStore({ onClick }) {
                         checked={data?.id === deliverCheck}
                         onChange={() => setDeliverCheck(data?.id)}
                       />
-                      <label htmlFor={data?.name_uz} className={` cursor-pointer md:px-3 w-fit border border-searchBgColor flex items-center justify-center   text-[10px] ls:text-[12px] md:text-base font-AeonikProRegular h-[32px] md:h-[42px] rounded-lg`}>
+                      <label htmlFor={data?.name_uz} className={`cursor-pointer md:px-3 w-[200px] border border-searchBgColor flex items-center justify-center   text-[10px] ls:text-[12px] md:text-base font-AeonikProRegular h-[32px] md:h-[42px] rounded-lg`}>
                         <span>{data?.name_ru}</span>
                       </label>
                     </>
