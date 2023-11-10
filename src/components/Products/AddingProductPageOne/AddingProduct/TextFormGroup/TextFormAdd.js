b99f0332526e121ac977ad4dd04a9ee2681f0055
MurodjohnKhayitov <murodillaxayitov@gmail.com>
2023-11-10 12:44:01 +0500

    all of the fixede
    all of the fixede

--- src/components/Products/AddingProductPageOne/AddingProduct/TextFormGroup/TextFormAdd.js
+++ src/components/Products/AddingProductPageOne/AddingProduct/TextFormGroup/TextFormAdd.js
@@ -8,17 +8,21 @@ import { dressMainData } from "../../../../../hook/ContextTeam";
 import AddBtn from "./AddBtn";
 
 export default function TextFormAdd({ onClick, handlCallBack }) {
+
     const [dressInfo, setDressInfo] = useContext(dressMainData);
-    const [titleInRu, setTitleInRu] = useState(null)
-    const [titleInUz, setTitleInUz] = useState(null)
-    const [descriptionInRu, setDescriptionInRu] = useState(null)
-    const [descriptionInUz, setDescriptionInUz] = useState(null)
-    const [qualityInRu, setQualityInRu] = useState(null)
-    const [qualityInUz, setQualityInUz] = useState(null)
-    const [noteValueRu, setNoteValueRu] = useState(null)
-    const [noteValueUz, setNoteValueUz] = useState(null)
-    const [isCheckValid, setIsCheckValid] = useState(false)
-    const [brand, setBrand] = useState(null)
+    const [state, setState] = useState({
+        titleInRu: null,
+        titleInUz: null,
+        descriptionInRu: null,
+        descriptionInUz: null,
+        qualityInRu: null,
+        qualityInUz: null,
+        noteValueRu: null,
+        noteValueUz: null,
+        isCheckValid: false,
+        brand: null,
+    })
+
 
 
 
@@ -27,14 +31,16 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
 
     const handleSelectQuality = (value) => {
         productsData?.quality?.filter(e => e.name_ru == value).map(item => {
-            setQualityInUz(item?.name_uz)
+            setState({ ...state, qualityInUz: item?.name_uz })
         })
-        setQualityInRu(value)
+        setState({ ...state, qualityInRu: value })
+
     }
     const handleSelectQualityUz = (value) => {
     }
     const handleBrand = (value) => {
-        setBrand(value)
+        setState({ ...state, brand: value })
+
     }
 
     useQuery(["products_get_page_next"], () => { return request({ url: "/products/get-product-info", token: true }) },
@@ -62,28 +68,27 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
 
 
     const send = () => {
-        setIsCheckValid(true)
-        if (titleInRu && titleInUz && qualityInRu && qualityInUz) {
-            handlCallBack({
-                brand_id: brand,
-                name_Ru: titleInRu,
-                name_Uz: titleInUz,
-                description_Ru: descriptionInRu,
-                description_Uz: descriptionInUz,
-                quality_Ru: qualityInRu,
-                quality_Uz: qualityInUz,
-                composition_Ru: noteValueRu,
-                composition_Uz: noteValueUz,
-            })
+        setState({ ...state, isCheckValid: true })
+
+        handlCallBack({
+            brand_id: state?.brand,
+            name_Ru: state?.titleInRu,
+            name_Uz: state?.titleInUz,
+            description_Ru: state?.descriptionInRu,
+            description_Uz: state?.descriptionInUz,
+            quality_Ru: state?.qualityInRu,
+            quality_Uz: state?.qualityInUz,
+            composition_Ru: state?.noteValueRu,
+            composition_Uz: state?.noteValueUz,
+        })
+        if (state?.titleInRu && state?.titleInUz && state?.qualityInRu && state?.qualityInRu) {
             onClick()
-            setIsCheckValid(false)
+            setState({ ...state, isCheckValid: false })
 
         }
     }
 
-    const handleCopyText = () => {
-        navigator.clipboard.writeText(titleInRu)
-    }
+
     // Категория одежды хлопок
     return (
         <div className="flex py-[40px] md:py-[50px] ">
@@ -121,18 +126,19 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                         <StarLabel />
                                     </span>
                                 </div>
-                                <div className={`justify-between rounded-lg   ${isCheckValid && !titleInRu ? "border border-[#FFB8B8] " : "border border-borderColor"}  flex py-[10px] px-[5px]`}>
+                                <div className={`justify-between rounded-lg   ${state?.isCheckValid && !state?.titleInRu ? "border border-[#FFB8B8] " : "border border-borderColor"}  flex py-[10px] px-[5px]`}>
                                     <input
                                         className="flex-1 mr-[30px] w-[30px] focus:outline-none font-AeonikProRegular"
                                         type="text"
                                         name="title"
-                                        value={titleInRu}
-                                        onChange={(e) => setTitleInRu(e.target.value)}
+                                        value={state?.titleInRu}
+                                        onChange={(e) => setState({ ...state, titleInRu: e.target.value })}
+
                                     />
 
                                     <button
                                         type="button"
-                                        onClick={() => navigator.clipboard.writeText(titleInRu)}
+                                        onClick={() => navigator.clipboard.writeText(state?.titleInRu)}
                                         className="cursor-pointer ">
                                         <AddBtn />
                                     </button>
@@ -147,17 +153,18 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                         <StarLabel />
                                     </span>
                                 </div>
-                                <div className={`justify-between rounded-lg   ${isCheckValid && !titleInUz ? "border border-[#FFB8B8] " : "border border-borderColor"}  flex py-[10px] px-[5px]`}>
+                                <div className={`justify-between rounded-lg   ${state?.isCheckValid && !state?.titleInUz ? "border border-[#FFB8B8] " : "border border-borderColor"}  flex py-[10px] px-[5px]`}>
                                     <input
                                         className="flex-1 mr-[30px] w-[30px] ll:w-auto focus:outline-none font-AeonikProRegular"
                                         type="text"
                                         name="title"
-                                        value={titleInUz}
-                                        onChange={(e) => setTitleInUz(e.target.value)}
+
+                                        value={state?.titleInUz}
+                                        onChange={(e) => setState({ ...state, titleInUz: e.target.value })}
                                     />
                                     <button
                                         type="button"
-                                        onClick={() => navigator.clipboard.writeText(titleInUz)}
+                                        onClick={() => navigator.clipboard.writeText(state?.titleInUz)}
                                         className="cursor-pointer ">
                                         <AddBtn />
                                     </button>
@@ -174,16 +181,18 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                 </div>
                                 <div className="rounded-lg relative border pr-[40px] border-[#e5e5e5] flex flex-col h-[120px] py-[10px] px-[5px]">
                                     <textarea
-                                        className="block w-full h-full text-[#666] text-sm resize-none bg-transparent flex-1 outline-none font-AeonikProRegular"
+                                        className="block w-full h-full text-[#666] text-[16px] resize-none bg-transparent flex-1 outline-none font-AeonikProRegular"
                                         name=""
                                         id=""
-                                        value={descriptionInRu}
-                                        onChange={(e) => setDescriptionInRu(e.target.value)}
+
+                                        value={state?.descriptionInRu}
+                                        onChange={(e) => setState({ ...state, descriptionInRu: e.target.value })}
+
                                     ></textarea>
                                     <div className="flex justify-end w-full absolute right-[6px] bottom-[6px]">
                                         <button
                                             type="button"
-                                            onClick={() => navigator.clipboard.writeText(descriptionInRu)}
+                                            onClick={() => navigator.clipboard.writeText(state?.descriptionInRu)}
                                             className="cursor-pointer ">
                                             <AddBtn />
                                         </button>
@@ -202,16 +211,17 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                 {/* error -  border-[#ffb8b8] bg-[#fff6f6] */}
                                 <div className="rounded-lg relative border pr-[40px] border-[#e5e5e5] flex flex-col h-[120px] py-[10px] px-[5px]">
                                     <textarea
-                                        className="block w-full h-full text-[#666] text-sm resize-none bg-transparent flex-1 outline-none font-AeonikProRegular"
+                                        className="block w-full h-full text-[#666] text-[16px] resize-none bg-transparent flex-1 outline-none font-AeonikProRegular"
                                         name=""
                                         id=""
-                                        value={descriptionInUz}
-                                        onChange={(e) => setDescriptionInUz(e.target.value)}
+
+                                        value={state?.descriptionInUz}
+                                        onChange={(e) => setState({ ...state, descriptionInUz: e.target.value })}
                                     ></textarea>
                                     <div className="flex justify-end w-full absolute right-[6px] bottom-[6px]">
                                         <button
                                             type="button"
-                                            onClick={() => navigator.clipboard.writeText(descriptionInUz)}
+                                            onClick={() => navigator.clipboard.writeText(state?.descriptionInUz)}
                                             className="cursor-pointer ">
                                             <AddBtn />
                                         </button>
@@ -236,7 +246,7 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                     </div>
                                     <Select
                                         placeholder={"Выбрать"}
-                                        className={`rounded-[6px] overflow-hidden ${isCheckValid && !qualityInRu ? "border border-[#FFB8B8] " : ""}  `}
+                                        className={`rounded-[6px] overflow-hidden ${state?.isCheckValid && !state?.qualityInRu ? "border border-[#FFB8B8] " : ""}  `}
                                         style={{ width: "100%" }}
                                         // value={lang === '' ? 'Выбрать' : lang}
                                         allowClear
@@ -265,11 +275,11 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                     </div>
                                     <Select
                                         placeholder={"Выбрать"}
-                                        className={`rounded-[6px] overflow-hidden ${isCheckValid && !qualityInUz ? "border border-[#FFB8B8] " : ""}  `}
+                                        className={`rounded-[6px] overflow-hidden ${state?.isCheckValid && !state?.qualityInRu ? "border border-[#FFB8B8] " : ""}  `}
 
                                         style={{ width: "100%" }}
                                         value={
-                                            productsData?.quality?.filter(e => e.name_ru == qualityInRu).map(item => {
+                                            productsData?.quality?.filter(e => e.name_ru == state?.qualityInRu).map(item => {
                                                 return item?.name_uz
                                             })
                                             // lang === ''
@@ -309,14 +319,14 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                         <input
                                             className="flex-1 mr-[30px] w-[30px] ll:w-auto focus:outline-none font-AeonikProRegular"
                                             type="text"
-                                            // name="note"
-                                            value={noteValueRu}
-                                            onChange={(e) => setNoteValueRu(e.target.value)}
+
+                                            value={state?.noteValueRu}
+                                            onChange={(e) => setState({ ...state, noteValueRu: e.target.value })}
                                         />
 
                                         <button
                                             type="button"
-                                            onClick={() => navigator.clipboard.writeText(noteValueRu)}
+                                            onClick={() => navigator.clipboard.writeText(state?.noteValueRu)}
                                             className="cursor-pointer ">
                                             <AddBtn />
                                         </button>
@@ -334,12 +344,13 @@ export default function TextFormAdd({ onClick, handlCallBack }) {
                                         <input
                                             className="flex-1 mr-[30px] w-[30px] ll:w-auto focus:outline-none font-AeonikProRegular"
                                             type="text"
-                                            value={noteValueUz}
-                                            onChange={(e) => setNoteValueUz(e.target.value)}
+
+                                            value={state?.noteValueUz}
+                                            onChange={(e) => setState({ ...state, noteValueUz: e.target.value })}
                                         />
                                         <button
                                             type="button"
-                                            onClick={() => navigator.clipboard.writeText(noteValueUz)}
+                                            onClick={() => navigator.clipboard.writeText(state?.noteValueUz)}
                                             className="cursor-pointer ">
                                             <AddBtn />
                                         </button>
