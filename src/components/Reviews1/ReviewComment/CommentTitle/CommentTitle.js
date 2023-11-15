53e5cf0750b517d1178d0f644daf91cc824ab24a
main <ismoilergashovich@gmail.com>
2023-11-15 13:43:38 +0500

    Commit store

--- src/components/Reviews1/ReviewComment/CommentTitle/CommentTitle.js
+++ src/components/Reviews1/ReviewComment/CommentTitle/CommentTitle.js
@@ -12,8 +12,8 @@ import { useParams } from "react-router-dom";
 
 const CommentTitle = ({ titleStore }) => {
   const { request } = useHttp();
-  const [commentStore, setCommnetStore] = useState(); 
-  const [sendText, setSendText] = useState(false); 
+  const [commentStore, setCommnetStore] = useState();
+  const [sendText, setSendText] = useState(false);
   const [state, setState] = useState({
     sendAnswer: false,
     sendText: false,
@@ -22,7 +22,7 @@ const CommentTitle = ({ titleStore }) => {
     getUserId: null,
     getComment: null,
   });
-  console.log(state.getComment, "COMMENT_GET");
+  console.log(titleStore, "titleStore");
 
   // ------------GET Has Reviews-STORE ?-----------------
   useQuery(
@@ -35,7 +35,7 @@ const CommentTitle = ({ titleStore }) => {
         if (res) {
           setCommnetStore(res.shops.data);
           console.log(res.shops.data, "Comments-STORE-Review");
-          setState({ ...state, getUserId: res });
+          // setState({ ...state, getUserId: res });
         }
       },
       onError: (err) => {
@@ -59,7 +59,7 @@ const CommentTitle = ({ titleStore }) => {
       },
       body: JSON.stringify({
         reply: state.replyText,
-        id: params.id,
+        id: state?.getUserId,
       }),
     })
       .then((res) => {
@@ -73,13 +73,21 @@ const CommentTitle = ({ titleStore }) => {
         console.log(data);
       });
   };
-
+  // Mrs. Lauren Wolff
   useEffect(() => {
     window.scrollTo({
       top: 0,
     });
   }, []);
-
+  const [replyText, setReplyText] = useState("");
+  useEffect(() => {
+    titleStore?.locationListId?.shop?.ratings?.map((item) => {
+      setReplyText(item?.reply);
+      setState({ ...state, getUserId: item?.id });
+    });
+  }, [titleStore || sendText]);
+  console.log(replyText, "replyText");
+  // console.log(state?.getUserId, "getUserId");
   return (
     <div className="w-full h-full  flex flex-col md:gap-y-[15px]">
       <div className="pb-1 md:justify-end text-tableTextTitle2 text-xl not-italic font-AeonikProMedium flex items-center md:gap-x-4 mt-[37px] mb-[18px] md:mt-0 md:mb-0">
@@ -191,78 +199,90 @@ const CommentTitle = ({ titleStore }) => {
                     </span>
                   </div>
                   {/* Comment Section */}
-                  {/* {state?.sendText ? ( */}
-                    <div className={`${sendText ? 'block' : 'hidden' } w-full h-fit mt-[20px] md:mt-[15px] md:p-[15px] `}>
-                      <div className="w-full h-fit flex justify-between px-[15px] py-3 md:p-[25px] bg-ProductReplyBg rounded-lg gap-x-[15px]">
-                        <div>
-                          <p className="text-tableTextTitle2 text-[12px] md:text-base font-AeonikProMedium mb-4">
-                            <span className="mr-1">Ответ</span>
-                            {titleStore?.locationListId?.shop?.name}
-                          </p>
-                          <p className="text-gray-700 text-[12px] md:text-base font-AeonikProRegular">
-                            {state?.getComment?.rating?.reply}
-                          </p>
-                        </div>
-                        <div className="flex items-start mt-[2px]">
-                          <span className="text-textLightColor text-[11px] md:text-base font-AeonikProRegular leading-normal">
-                            {item?.replyDate}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  {/* ) : (
-                    ""
-                  )} */}
-                  <form
-                    onSubmit={(e) => e.preventDefault()}
-                    className={`${sendText ? 'hidden' : 'flex '} w-full h-fit mt-[25px] md:mt-[5px]  justify-end`}
-                  >
-                    {state?.sendAnswer ? (
-                      <div className="w-full flex flex-col md:flex-row items-center justify-between">
-                        <textarea
-                          name="answer"
-                          id="answer"
-                          className="w-full md:w-4/5 h-12 text-[13px] md:text-base md:h-14 border rounded-lg p-3 md:mr-[20px] xxl:mr-[30px]"
-                          value={state?.replyText}
-                          onChange={(e) =>
-                            setState({ ...state, replyText: e.target.value })
-                          }
-                          placeholder="Add your answer..."
-                        ></textarea>
-                        <div className="flex items-center ml-auto mt-3 md:mt-0">
-                          <button
-                            onClick={() => {
-                              sendReply();
-                              // setState({ ...state, sendText: false })
-                              setSendText(!sendText)
-                            }}
-                            className={`w-[132px] h-9 md:py-0 md:h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg mr-[10px]`}
-                          >
-                            <span className="text-[13px] md:text-sm not-italic font-AeonikProMedium">
-                              Отправить
+                  {titleStore?.locationListId?.shop?.ratings?.length !== 0 &&
+                    replyText && (
+                      <div className={`w-full h-fit mt-[20px] md:mt-[15px] md:p-[15px]`}>
+                        <div className="w-full h-fit flex justify-between px-[15px] py-3 md:p-[25px] bg-ProductReplyBg rounded-lg gap-x-[15px]">
+                          <div>
+                            <p className="text-tableTextTitle2 text-[12px] md:text-base font-AeonikProMedium mb-4">
+                              <span className="mr-1">Ответ</span>
+                              {titleStore?.locationListId?.shop?.name}
+                            </p>
+                            <p className="text-gray-700 text-[12px] md:text-base font-AeonikProRegular">
+                              {/* {state?.getComment?.rating?.reply} */}
+                              {titleStore?.locationListId?.shop?.ratings?.map(
+                                (item) => {
+                                  return (
+                                    <span>{item?.reply || "null reply"}</span>
+                                  );
+                                }
+                              )}
+                            </p>
+                          </div>
+                          <div className="flex items-start mt-[2px]">
+                            <span className="text-textLightColor text-[11px] md:text-base font-AeonikProRegular leading-normal">
+                              {item?.replyDate}
                             </span>
-                          </button>
-                          <button
-                            onClick={() =>
-                              setState({ ...state, sendAnswer: false })
-                            }
-                            className="w-9 h-9 md:w-11 md:h-11 bg-white flex items-center justify-center active:scale-95  active:opacity-70 text-white border border-textBlueColor rounded-lg"
-                          >
-                            <CloseAnswer colors="#007DCA" />
-                          </button>
+                          </div>
                         </div>
                       </div>
-                    ) : (
-                      <button
-                        onClick={() => setState({ ...state, sendAnswer: true })}
-                        className="w-full md:w-[132px] h-9 md:py-0 md:h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg"
-                      >
-                        <span className="text-[13px] md:text-sm not-italic font-AeonikProMedium">
-                          Ответить
-                        </span>
-                      </button>
                     )}
-                  </form>
+                  {!replyText && (
+                    <form
+                      onSubmit={(e) => e.preventDefault()}
+                      className={`${
+                        sendText ? "hidden" : "flex "
+                      } w-full h-fit mt-[25px] md:mt-[5px]  justify-end`}
+                    >
+                      {state?.sendAnswer ? (
+                        <div className="w-full flex flex-col md:flex-row items-center justify-between">
+                          <textarea
+                            name="answer"
+                            id="answer"
+                            className="w-full md:w-4/5 h-12 text-[13px] md:text-base md:h-14 border rounded-lg p-3 md:mr-[20px] xxl:mr-[30px]"
+                            value={state?.replyText}
+                            onChange={(e) =>
+                              setState({ ...state, replyText: e.target.value })
+                            }
+                            placeholder="Add your answer..."
+                          ></textarea>
+                          <div className="flex items-center ml-auto mt-3 md:mt-0">
+                            <button
+                              onClick={() => {
+                                sendReply();
+                                // setState({ ...state, sendText: false })
+                                setSendText(!sendText);
+                              }}
+                              className={`w-[132px] h-9 md:py-0 md:h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg mr-[10px]`}
+                            >
+                              <span className="text-[13px] md:text-sm not-italic font-AeonikProMedium">
+                                Отправить
+                              </span>
+                            </button>
+                            <button
+                              onClick={() =>
+                                setState({ ...state, sendAnswer: false })
+                              }
+                              className="w-9 h-9 md:w-11 md:h-11 bg-white flex items-center justify-center active:scale-95  active:opacity-70 text-white border border-textBlueColor rounded-lg"
+                            >
+                              <CloseAnswer colors="#007DCA" />
+                            </button>
+                          </div>
+                        </div>
+                      ) : (
+                        <button
+                          onClick={() =>
+                            setState({ ...state, sendAnswer: true })
+                          }
+                          className="w-full md:w-[132px] h-9 md:py-0 md:h-11 bg-textBlueColor flex items-center justify-center active:scale-95  active:opacity-70 text-white rounded-lg"
+                        >
+                          <span className="text-[13px] md:text-sm not-italic font-AeonikProMedium">
+                            Ответить
+                          </span>
+                        </button>
+                      )}
+                    </form>
+                  )}
                 </div>
               );
             })}
