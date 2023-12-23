f7f9819d2100aa40f6891ee776294374d28672f5
MurodjohnKhayitov <murodillaxayitov@gmail.com>
2023-12-23 14:36:57 +0500

    new change
    new change

--- src/components/Products/AddingProductEdit/AddingProduct/Details/Shoes/ShoesAdd.js
+++ src/components/Products/AddingProductEdit/AddingProduct/Details/Shoes/ShoesAdd.js
@@ -428,11 +428,12 @@ function ShoesAdd({ stateList, colorsList, ColorModal, addNewColor, DeleteSize, 
                 {checked?.length ?
                     <button type="button" onClick={ColorModal} className="text-textBlueColor flex items-center gap-x-1 hover:underline text-base not-italic font-AeonikProMedium">
                         <span> Добавить к цвету</span>
-                        <div
-                            style={{ background: `${addNewColor.hex}` }}
+                        {addNewColor && <div
+                            style={{ background: `${addNewColor?.hex}` }}
                             className={`w-[22px] h-[22px] flex items-center justify-center rounded-full ${addNewColor?.id === 2 ? "border " : ""}`}
                         >
-                        </div>                    </button>
+                        </div>}
+                    </button>
                     :
                     <span className="text-[#b5b5b5]  text-base not-italic font-AeonikProMedium">
                         Добавить к цвету
--- src/components/Products/AddingProductEdit/AddingProduct/Details/UnderAddWear/UnderAddWear.js
+++ src/components/Products/AddingProductEdit/AddingProduct/Details/UnderAddWear/UnderAddWear.js
@@ -751,11 +751,11 @@ function UnderAddWear({ stateList, colorsList, ColorModal, addNewColor, DeleteSi
                 {checked?.length ?
                     <button type="button" onClick={ColorModal} className="text-textBlueColor flex items-center gap-x-1 hover:underline text-base not-italic font-AeonikProMedium">
                         <span> Добавить к цвету</span>
-                        <div
-                            style={{ background: `${addNewColor.hex}` }}
+                        {addNewColor && <div
+                            style={{ background: `${addNewColor?.hex}` }}
                             className={`w-[22px] h-[22px] flex items-center justify-center rounded-full ${addNewColor?.id === 2 ? "border " : ""}`}
                         >
-                        </div>
+                        </div>}
                     </button>
                     :
                     <span className="text-[#b5b5b5]  text-base not-italic font-AeonikProMedium">
