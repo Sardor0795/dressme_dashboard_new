ab6117d218a184ad4f8182c4686a42a5c9314fb1
MurodjohnKhayitov <murodillaxayitov@gmail.com>
2024-01-23 17:26:55 +0500

    data
    data

--- src/components/Authentication/SellerAuthentication/forgotPassword/ForgotPasswordSeller.js
+++ src/components/Authentication/SellerAuthentication/forgotPassword/ForgotPasswordSeller.js
@@ -193,7 +193,7 @@ export default function ForgotPasswordSeller() {
                         className="mt-8  border cursor-pointer flex items-center justify-center border-searchBgColor w-full h-12 bg-textBlueColor select-none rounded-lg active:scale-95	active:opacity-70 "
                     >
                         <span className="not-italic font-AeonikProMedium mr-2 text-base leading-4 text-center text-white tracking-[0,16px]">
-                            Сбросит пароль{" "}
+                            Сбросить пароль{" "}
                         </span>
                         <span>
                             <SircleNext colors={"#fff"} />
