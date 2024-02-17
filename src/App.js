231be166614561eb99d64ae05337732a12ecd5d6
MurodjohnKhayitov <murodillaxayitov@gmail.com>
2024-02-17 09:35:37 +0500

    new changes
    new changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React, { useContext, useEffect, useState } from "react";
 import RouterList from "./root/RouterList";
 import { dressMainData } from "./hook/ContextTeam";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { HelperData } from "./hook/HelperDataStore";
+import { SellerRefresh } from "./hook/SellerRefreshToken";
 const { REACT_APP_BASE_URL } = process.env;
 
 function App() {
   const [dressInfo, setDressInfo] = useContext(dressMainData);
+  const [helperDatainform, setHelperDatainform] = useContext(HelperData);
+  const [sellerRefreshToken] = useContext(SellerRefresh)
 
   useEffect(() => {
     const fetchDataRegions = async () => {
@@ -17,7 +22,9 @@ function App() {
         }
 
       } catch (error) {
-
+        if (error?.response?.status === 401) {
+          sellerRefreshToken()
+        }
       }
     };
 
@@ -25,7 +32,48 @@ function App() {
       fetchDataRegions();
     }
 
-  }, []); return (
+  }, []);
+
+  const fetchData = async (customHeaders) => {
+    try {
+      const response = await axios.get(`${REACT_APP_BASE_URL}/shops`, {
+        headers: customHeaders,
+      });
+      const status = response.status;
+      const data = response.data;
+
+      return { data, status };
+    } catch (error) {
+      const status = error.response ? error.response.status : null;
+      return { error, status };
+    }
+  };
+
+  const customHeaders = {
+    'Content-type': 'application/json; charset=UTF-8',
+    "Authorization": `Bearer ${localStorage.getItem("DressmeUserToken")}`,    // Add other headers as needed
+  };
+  useQuery(['seller_shops_list'], () => fetchData(customHeaders), {
+    onSuccess: (data) => {
+      if (data?.status >= 200 && data?.status < 300) {
+        setHelperDatainform({ ...helperDatainform, shopsList: data?.data })
+      }
+
+      if (data?.status === 401) {
+        sellerRefreshToken()
+        fetchData()
+      }
+    },
+    onError: (error) => {
+      if (error?.response?.status === 401) {
+        sellerRefreshToken()
+      }
+    },
+    keepPreviousData: true,
+    refetchOnWindowFocus: false,
+  });
+
+  return (
     <div>
       {/* <NavbarDashboard /> */}
       <RouterList />
