21dcfeecff293f7217f18f24f9008a0fa50adf27
main <ismoilergashovich@gmail.com>
2024-02-28 19:56:17 +0500

    dashboar product add mobile version changes

--- src/components/Products/AddingProductPageOne/ProductsPageOne.js
+++ src/components/Products/AddingProductPageOne/ProductsPageOne.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import AddingProduct from "./AddingProduct/AddingProduct";
-import MobileHumburgerMenu from "../../Navbar/mobileHamburgerMenu/MobileMenu";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AiOutlineLeft } from "react-icons/ai";
 import { GoBackIcons } from "../../../assets/icons";
 import { dressMainData } from "../../../hook/ContextTeam";
@@ -18,42 +17,52 @@ export default function ProductsPageOne() {
 
   return (
     <div>
-      <div className="w-full pt-6 md:pb-4   md:py-4 md:border-b border-lightBorderColor block">
-
-        <div className="flex items-center md:justify-between mb-6 md:mb-0">
-          <section className="w-full md:w-fit  flex justify-center md:justify-start">
-            {dressInfo?.nextPageShowForm && <div className="absolute left-4 md:hidden flex">
-              <MobileHumburgerMenu />
-            </div>}
-            <section className=" w-full md:w-fit  flex justify-center md:justify-start">
-              {dressInfo?.isCheckPoructList?.length >= 1 ? <button
-                onClick={() => {
-                  navigate(-1);
-                }}
-                className="w-8 h-8 md:mr-[30px] flex items-center cursor-pointer justify-center border border-borderColor rounded-lg active:scale-95  active:opacity-70"
-              >
-                <AiOutlineLeft />
-              </button>
-                :
-                !dressInfo?.nextPageShowForm && <> <button
-                  button
-                  onClick={() => setDressInfo({ ...dressInfo, nextPageShowForm: true })}
-                  className="w-8 h-8 hidden md:flex items-center cursor-pointer justify-center border border-borderColor rounded-lg"
+      <div className="w-full pt-6 md:pb-4 md:py-4 md:border-b border-lightBorderColor block">
+        <div className="w-full flex items-center md:justify-between mb-6 md:mb-0">
+          <section className="w-full md:w-fit flex justify-center md:justify-start">
+            <section className="w-full md:w-fit flex justify-start">
+              {dressInfo?.isCheckPoructList?.length >= 1 ? (
+                <button
+                  onClick={() => {
+                    navigate(-1);
+                  }}
+                  className="w-8 h-8 md:mr-[30px] flex items-center cursor-pointer justify-center border border-borderColor rounded-lg active:scale-95  active:opacity-70"
                 >
                   <AiOutlineLeft />
                 </button>
-                  <button
-                    onClick={() => setDressInfo({ ...dressInfo, nextPageShowForm: true })}
-                    className="  md:hidden absolute left-0 flex items-center cursor-pointer "
-                  >
-                    <GoBackIcons />
-                  </button></>}
-              <p className={`text-black text-[20px] ll:text-2xl not-italic font-AeonikProMedium ${!dressInfo?.nextPageShowForm && "md:ml-[30px]"} `}>
+              ) : (
+                !dressInfo?.nextPageShowForm && (
+                  <>
+                    {" "}
+                    <button
+                      button
+                      onClick={() =>
+                        setDressInfo({ ...dressInfo, nextPageShowForm: true })
+                      }
+                      className="w-8 h-8 hidden md:flex items-center cursor-pointer justify-center border border-borderColor rounded-lg"
+                    >
+                      <AiOutlineLeft />
+                    </button>
+                    <button
+                      onClick={() =>
+                        setDressInfo({ ...dressInfo, nextPageShowForm: true })
+                      }
+                      className="  md:hidden absolute left-0 flex items-center cursor-pointer "
+                    >
+                      <GoBackIcons />
+                    </button>
+                  </>
+                )
+              )}
+              <p
+                className={`w-full text-center text-black text-[20px] ll:text-2xl not-italic font-AeonikProMedium ${
+                  !dressInfo?.nextPageShowForm && "md:ml-[30px]"
+                } `}
+              >
                 Добавить товара
               </p>
             </section>
           </section>
-
         </div>
       </div>
       <div className="w-full max-w-[1540px] mx-auto ">
@@ -61,7 +70,6 @@ export default function ProductsPageOne() {
           <AddingProduct />
         </div>
       </div>
-    </div >
-
+    </div>
   );
 }
