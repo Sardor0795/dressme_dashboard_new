cdee5f1b229c9ac5720085dd75944bb67c14b884
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2024-03-23 22:27:30 +0500

    new changes
    new changes

--- src/components/Authentication/AxiosIntance.js
+++ src/components/Authentication/AxiosIntance.js
@@ -1,76 +1,62 @@
 import axios from 'axios';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { dressMainData } from '../../hook/ContextTeam';
-import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 
+
+// Creating an Axios instance with the base URL for the API
 const axiosInstance = axios.create({
     baseURL: "https://api.dressme.uz/api/seller", // Replace with your API base URL
 });
-
+// Flag to keep track of the token refresh process
 let isRefreshing = false;
+// Array to store refresh subscribers (callbacks)
 let refreshSubscribers = [];
-let reloadCounter = 0;
-
-// const [dressInfo, setDressInfo] = useContext(dressMainData);
-// async function refreshToken() {
-//     try {
-//         const headers = {
-//             'Content-type': 'application/json; charset=UTF-8',
-//             "Authorization": `Bearer ${localStorage.getItem("RefreshUserToken")}`,
-//         };
-//         const data = {
-//             refresh_token: localStorage.getItem("RefreshUserToken"),
-//         };
-//         const response = await axios.post(`https://api.dressme.uz/api/seller/refresh-token`, data, { headers });
-//         if (response?.status >= 200 && response?.status < 300) {
-//             localStorage.setItem("DressmeUserToken", response?.data?.access_token);
-//         }
-//     } catch (error) {
-//         if (error?.response?.status === 401 || error?.response?.status === 403) {
-//             localStorage.removeItem("DressmeUserToken");
-//             // const navigate = useNavigate();
-//             //  console.log("expired refresdh token", error?.response?.status);
-//         }
-//     }
-// }
-
-// Function to reload the page only once
 
 
+// Function to refresh the access token
 async function refreshToken() {
     try {
+        // Prepare headers for the request
         const headers = {
             'Content-Type': 'application/json',
             "Authorization": `Bearer ${localStorage.getItem("RefreshUserToken")}`,
         };
+        // Prepare data for the request
         const data = {
             refresh_token: localStorage.getItem("RefreshUserToken"),
         };
+        // Send request to refresh the access token
         const response = await axios.post(`https://api.dressme.uz/api/seller/refresh-token`, data, { headers });
         if (response?.status >= 200 && response?.status < 300) {
+            // Update local storage with the new access token
             localStorage.setItem("DressmeUserToken", response?.data?.access_token);
             // Resolve all refresh subscribers with the new access token
             refreshSubscribers.forEach(callback => callback(response.data.access_token));
             // Clear refresh subscribers
             refreshSubscribers = [];
+
             return response.data.access_token;
         }
     } catch (error) {
         if (error?.response?.status === 401 || error?.response?.status === 403) {
+            // Remove the access token from local storage
             localStorage.removeItem("DressmeUserToken");
-             <Navigate to="/login-seller" replace />;
+            // Navigate to the login page and reload the app
+            <Navigate to="/login-seller" replace />;
             if (localStorage?.getItem("DressmeUserToken")) {
                 window.location.reload();
             }
         }
-        throw error; // Propagate the error for further handling
+        // Propagate the error for further handling
+        throw error;
     }
 }
-
+// Configure Axios to add the access token to the request headers
 axiosInstance.interceptors.request.use(
     (config) => {
+        // Get the access token from local storage
         const accessToken = localStorage.getItem('DressmeUserToken');
         if (accessToken) {
+            // Add the access token to the request headers
             config.headers.Authorization = `Bearer ${accessToken}`;
         }
         return config;
@@ -90,11 +76,22 @@ axiosInstance.interceptors.response.use(
         if (error.response && error.response.status === 401 && !originalRequest._retry) {
             if (!isRefreshing) {
                 isRefreshing = true;
-                await refreshToken();
-                isRefreshing = false;
+                try {
+                    const accessToken = await refreshToken();
+                    // Update the request headers with the new access token
+                    originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+                    // Retry the original request
+                    return axiosInstance(originalRequest);
+                } catch (refreshError) {
+                    return Promise.reject(refreshError);
+                } finally {
+                    isRefreshing = false;
+                }
             }
+            // If the token refresh process is already in progress, queue the original request
             return new Promise((resolve, reject) => {
-                refreshSubscribers.push(() => {
+                refreshSubscribers.push((accessToken) => {
+                    originalRequest.headers.Authorization = `Bearer ${accessToken}`;
                     resolve(axiosInstance(originalRequest));
                 });
             });
@@ -104,3 +101,37 @@ axiosInstance.interceptors.response.use(
 );
 
 export default axiosInstance;
+
+// axiosInstance.interceptors.response.use(
+//     (response) => {
+//         return response;
+//     },
+//     async (error) => {
+//         const originalRequest = error.config;
+//         // console.log(error.response, error.response.status, 'error.response.status', !originalRequest._retry);
+//         if (error.response && error.response.status === 401 && !originalRequest._retry) {
+//             if (!isRefreshing) {
+//                 isRefreshing = true;
+//                 try {
+//                     const accessToken = await refreshToken();
+//                     // Update the request headers with the new access token
+//                     originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+//                     // Retry the original request
+//                     return axiosInstance(originalRequest);
+//                 } catch (refreshError) {
+//                     return Promise.reject(refreshError);
+//                 } finally {
+//                     isRefreshing = false;
+//                 }
+//             }
+//             // If the token refresh process is already in progress, queue the original request
+//             return new Promise((resolve, reject) => {
+//                 refreshSubscribers.push((accessToken) => {
+//                     originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+//                     resolve(axiosInstance(originalRequest));
+//                 });
+//             });
+//         }
+//         return Promise.reject(error);
+//     }
+// );
